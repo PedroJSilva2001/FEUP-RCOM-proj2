f47f01a958544193c9575cd71b4bb5de51ffca0c
Catarina Pires <catarina.o.pires@hotmail.com>
2022-01-16 22:57:25 +0000

    Added partial documentation in ftpconn

--- src/ftpconn.h
+++ src/ftpconn.h
@@ -6,6 +6,9 @@
 #include <stdio.h>
 #include <regex.h>
 
+/**
+ * Client information.
+ **/
 typedef struct {
   char *user;
   char *pass;
@@ -13,14 +16,38 @@ typedef struct {
   char *path;
 } ftp_client_info;
 
+/** @brief 
+ *  @param host Host.
+ *  @param port Port.
+ *  @return 
+ */
 struct addrinfo *host_IPaddrinfos(char *host, char *port);
 
+/** @brief Connects to host.
+ *  @param info The ftp_client_info struct.
+ *  @return Returns file descriptor of socket upon success, -1 otherwise.
+ */
 int connect_to_host(ftp_client_info *info);
 
+/** @brief 
+ *  @param capt_group
+ *  @param url
+ *  @return 
+ */
 char *get_client_param(regmatch_t capt_group, const char *url);
 
+/** @brief Parses given URL and updates ftp_client_info struct.
+ *  @param info The ftp_client_info struct.
+ *  @param url Given URL.
+ *  @return Returns 0 upon success, 1 otherwise.
+ */
 int parse_URL(ftp_client_info *info, const char *url);
 
+/** @brief 
+ *  @param ip Ip.
+ *  @param port Port.
+ *  @return Returns file descriptor of data socket.
+ */
 int connect_to_host_data_port(unsigned char *ip, unsigned char *port);
 
 #endif
